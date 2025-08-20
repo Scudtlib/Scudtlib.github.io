901fa5aaf7e92373d776fc40614087ac3f89b5fa
Scudtlib <rita51525150@gmail.com>
2025-08-20 09:37:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+window.__firebase_config = JSON.stringify({
+  apiKey: "AIzaSyAtuavqeRz-THMItzfBSm-L-OCzDMIZMY",
+  authDomain: "wos-quiz.firebaseapp.com",
+  projectId: "wos-quiz",
+  appId: "1:905547725730:web:34033c1008a11276b8d0fb"
+});
+
+window.__app_id = "wos-quiz-demo";
